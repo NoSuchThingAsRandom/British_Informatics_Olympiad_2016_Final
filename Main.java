ea34d9b95a513a2a7e5ba8cffe5cbf44ff7862ae
NoSuchThingAsRandom <samralph76@gmail.com>
2017-11-14 18:42:09 +0000

    Cleaned up and commented

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static void agentAnonymousPrequel(){
+        //Sets up
         Scanner in = new Scanner(System.in);
         System.out.println("Agents Anonymous Prequel, Enter using commaK");
         int numSpies = Integer.parseInt(in.nextLine());
@@ -12,30 +13,44 @@ public class Main {
         BitSet validSpies=new BitSet(numSpies);
         validSpies.flip(0,numSpies);
         ArrayList<int[]> spiesWhoKnowEachOther = new ArrayList<>();
-        while (!Objects.equals(line = in.nextLine(), "-1,-1")) {
 
+        //Gets inputs
+        while (!Objects.equals(line = in.nextLine(), "-1,-1")) {
             spiesWhoKnowEachOther.add(new int[]{Integer.parseInt((line.split(","))[0]),Integer.parseInt((line.split(","))[1])});
         }
+        //Removes all spies who know each other
         for(int currentSpy=0;currentSpy<numSpies;currentSpy++){
             if(validSpies.get(currentSpy)) {
-                ArrayList<Integer> thisSpyKnows = new ArrayList<Integer>();
                 for (int[] x : spiesWhoKnowEachOther) {
                     if (x[0] == currentSpy) {
-                        thisSpyKnows.add(x[1]);
                         validSpies.set(x[1], false);
                     }
                     if (x[1] == currentSpy) {
-                        thisSpyKnows.add(x[0]);
                         validSpies.set(x[0], false);
                     }
                 }
             }
         }
+        //Output -Calculates valid spies
+        int count=0;
+        ArrayList<Integer> valid=new ArrayList<>();
         for(int x=0;x<validSpies.size();x++){
             if (validSpies.get(x)){
-                System.out.println("This spy is valid: "+(x+1));
+                count++;
+                valid.add((x+1));
             }
         }
+        //Prints results
+        System.out.println(count);
+        for (int out:valid){
+            System.out.println(out);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        agentAnonymousPrequel();
 
     }
 }
